6adc3d3f503fe57768499277e385fe02efb4bcf6
Ayomide Owolana <ayomideowolana1@gmail.com>
2021-12-28 20:57:06 +0100

    configuration settings

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      title: "Forcaster",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  mode: "production",
+};
